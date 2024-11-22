845d332f3f2fb7c6bf46abfabc927ce8bd46e018
Lime <demn2001@gmail.com>
2024-11-22 22:18:01 +0300

    setup web

---
+++ src/TaskAGSR/ConfigureServices.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using TaskAGSR.Swagger;
+
+namespace TaskAGSR;
+
+public static class ConfigureServices
+{
+    private static void ConfigureJsonOptions(this JsonSerializerOptions options)
+    {
+        options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+        options.NumberHandling = JsonNumberHandling.AllowReadingFromString;
+
+        options.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.SnakeCaseLower));
+    }
+
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services
+            .Configure<JsonOptions>(v => v.SerializerOptions.ConfigureJsonOptions())
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen(v =>
+            {
+                v.SupportNonNullableReferenceTypes();
+                v.SchemaFilter<SwaggerSchemaDefaultFilter>();
+                v.OperationFilter<SwaggerRequestBodyFilter>();
+            });
+
+        services.AddControllers()
+            .AddJsonOptions(v => v.JsonSerializerOptions.ConfigureJsonOptions());
+
+        return services;
+    }
+}
--- src/TaskAGSR/Program.cs
+++ src/TaskAGSR/Program.cs
@@ -1,22 +1,48 @@
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using TaskAGSR;
+using TaskAGSR.Application;
+using TaskAGSR.Infrastructure;
+using TaskAGSR.Infrastructure.DataBase;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", Serilog.Events.LogEventLevel.Warning)
+    .WriteTo.Console()
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog();
+builder.Services.AddSingleton(Log.Logger);
 
+builder.Services
+    .AddWebServices()
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration);
+
+builder.Services
+    .AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(builder.Configuration.GetRequiredSection("DataProtection").GetValue<string>("Path")!))
+    .UseCryptographicAlgorithms(new AuthenticatedEncryptorConfiguration
+    {
+        EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+        ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+    });
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (IServiceScope scope = app.Services.CreateScope())
+await using (ApplicationDbContext context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>())
+    await context.Database.MigrateAsync();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseAuthorization();
 
 app.MapControllers();
 
