c0cce1e532d572edda517ddd1f237d67f1fac2e0
Lime <demn2001@gmail.com>
2024-11-23 17:05:38 +0300

    add search MediatR query

---
+++ src/TaskAGSR.Application/Data/Queries/PatientSearchQuery.cs
@@ -0,0 +1,30 @@
+﻿using Hl7.Fhir.Search;
+using MediatR;
+using TaskAGSR.Application.Common.Interfaces;
+using TaskAGSR.Application.Common.VM;
+using TaskAGSR.Domain.Entities;
+
+namespace TaskAGSR.Application.Data.Queries;
+
+public record PatientSearchByBirthDateQuery(IEnumerable<ISearch<DateTime>> Filter) : IRequest<IEnumerable<PatientVM<PatientIndexIdentityVM>>>;
+
+public class PatientSearchQueryHandler(
+    IPatientRepository patientRepository) :
+    IRequestHandler<PatientSearchByBirthDateQuery, IEnumerable<PatientVM<PatientIndexIdentityVM>>>
+{
+    public async Task<IEnumerable<PatientVM<PatientIndexIdentityVM>>> Handle(PatientSearchByBirthDateQuery request, CancellationToken cancellationToken)
+        => (await patientRepository.SearchByBirthDateAsync(request.Filter, cancellationToken))
+        .Select(patient => new PatientVM<PatientIndexIdentityVM>
+        {
+            Name = new PatientIndexIdentityVM
+            {
+                Id = patient.Id,
+                Use = patient.Use,
+                Family = patient.Family,
+                Given = patient.Given,
+            },
+            Gender = patient.Gender,
+            BirthDate = patient.BirthDate,
+            Active = patient.Active,
+        });
+}
