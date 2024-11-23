9d26512f8f4457e84cab5b89d08614a1a1947888
Lime <demn2001@gmail.com>
2024-11-23 17:06:50 +0300

    create `GET patient/search?birth_date=...`

---
+++ src/TaskAGSR/Controllers/PatientSearchController.cs
@@ -0,0 +1,28 @@
+﻿using Hl7.Fhir.Search;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TaskAGSR.Application.Common.VM;
+using TaskAGSR.Application.Data.Queries;
+using TaskAGSR.Models.Response;
+
+namespace TaskAGSR.Controllers;
+
+[ApiController]
+[Route("patient/search")]
+public class PatientSearchController(IMediator mediator) : ControllerBase
+{
+    /// <summary>
+    /// Get patients record by birth date with using format Hl7.Fhir.Search#DateTime
+    /// </summary>
+    /// <param name="birthDate">Search by birth date list with using format Hl7.Fhir.Search#DateTime</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
+    /// <response code="200">Return patients record</response>
+    /// <response code="500">Internal server error</response>
+    [HttpGet]
+    [ProducesResponseType(typeof(SuccessResponse<IEnumerable<PatientVM<PatientIndexIdentityVM>>>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetBirthDateSearch(
+        [FromQuery(Name = "birth_date")] IEnumerable<DateTimeSearch> birthDate,
+        CancellationToken cancellationToken)
+        => Ok(SuccessResponse.Create(await mediator.Send(new PatientSearchByBirthDateQuery(birthDate), cancellationToken)));
+}
